93e70a82d9aeb77718cd3e93b8d97ca3811a4c66
qdonnellan <qdonnellan@gmail.com>
2013-11-16 14:01:43 -0600

    added problem 6

--- 1_25/05.py
+++ 1_25/05.py
@@ -7,9 +7,11 @@
 n = 20
 i = 20
 while i > 10:
+    # if the remainder is zero search for next lowest until less than 10
     if  n % i == 0:
-        i -= 1
+        i -= 1 
     else:
+    # if the remainder is not zero, restart search at 20 and increment by 20
         i = 20
         n += 20
 print n
