82157da62d7397db5cbb0aaa150e249bbd689341
qdonnellan <qdonnellan@gmail.com>
2013-11-16 13:50:21 -0600

    added problem 5

---
+++ 1_25/05.py
@@ -0,0 +1,16 @@
+# 2520 is the smallest number that can be divided by each of the numbers 
+# from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all 
+# of the numbers from 1 to 20?
+import numpy as np
+
+n = 20
+i = 20
+while i > 10:
+    if  n % i == 0:
+        i -= 1
+    else:
+        i = 20
+        n += 20
+print n
