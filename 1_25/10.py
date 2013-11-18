9fc22cfed0e9ac2030734d181acba0b5a6e73cf9
qdonnellan <qdonnellan@gmail.com>
2013-11-17 20:56:24 -0600

    problem 10 added, really slow right now

---
+++ 1_25/10.py
@@ -0,0 +1,17 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+# I'm using my solution to problem #7 to help me here
+
+n = 1
+the_sum = 0
+while n <=  2000000:
+    s = 2
+    n += 1
+    while n % s != 0 and s*s < n: # only check until s*s < n
+        s += 1
+    if s*s > n: # if n is prime to s*s > n, it will be prime
+        the_sum += n
+    
+print the_sum
