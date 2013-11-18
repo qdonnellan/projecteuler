72d50544c07adc3c6f6b3d662e6164c0b2cbe5d7
qdonnellan <qdonnellan@gmail.com>
2013-11-17 20:59:10 -0600

    a bit faster... still not very fast (problem 10)

--- 1_25/10.py
+++ 1_25/10.py
@@ -4,11 +4,11 @@
 
 # I'm using my solution to problem #7 to help me here
 
-n = 1
-the_sum = 0
-while n <=  2000000:
+n = 5
+the_sum = n
+while n < 2000000:
     s = 2
-    n += 1
+    n += 2
     while n % s != 0 and s*s < n: # only check until s*s < n
         s += 1
     if s*s > n: # if n is prime to s*s > n, it will be prime
