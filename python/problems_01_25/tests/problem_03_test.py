5bf8d6f5ae7954016fcb48063fa8b75be9af77c7
qdonnellan <qdonnellan@gmail.com>
2014-03-19 22:06:53 -0500

    added a test to problem 3

---
+++ python/problems_01_25/tests/problem_03_test.py
@@ -0,0 +1,19 @@
+from problems_01_25.problem_03 import largest_prime_factor
+import unittest
+
+class Problem03Test(unittest.TestCase):
+    """
+    test the implementation of Project Euler Problem 03
+    """
+
+    def test_case_where_n_equals_10(self):
+        """
+        the largest prime factor of 10 is 5
+        """
+        self.assertEqual(largest_prime_factor(10), 5)
+
+    def test_case_where_n_equals_13195(self):
+        """
+        the largest prime factor of 13195 is 29
+        """
+        self.assertEqual(largest_prime_factor(13195),29)
