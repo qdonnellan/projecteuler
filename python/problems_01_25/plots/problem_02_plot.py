70e37fbdfacb2da5896afd441f5b440b1e9b5f6e
qdonnellan <qdonnellan@gmail.com>
2014-03-19 22:06:22 -0500

    used the useful function array_from_function

--- python/problems_01_25/plots/problem_02_plot.py
+++ python/problems_01_25/plots/problem_02_plot.py
@@ -1,21 +1,23 @@
 from problems_01_25.problem_02 import even_fibsum
+from problems_01_25.plots.useful import array_from_function
 import matplotlib.pyplot as plt
 import numpy as np
 
 # plot the sum of even integers in a 
 # Fibbonicci series where the largest
 # term is less than the limit below
-LIMIT = 100000
+LIMIT = 10000
 
 x = np.arange(0,LIMIT)
-y = []
-for xi in x:
-    y.append(even_fibsum(xi))
-    
-y = np.array(y)
+y = array_from_function(x, even_fibsum)
 
 plt.xlabel('Largest term in Fib. Series')
 plt.ylabel('Sum of terms')
+plt.title(
+    """
+    Sum of even terms in a Fibonacci series
+    """
+)
 plt.plot(x,y, '-')
 
 # this graph looks great in a log-log scale!
