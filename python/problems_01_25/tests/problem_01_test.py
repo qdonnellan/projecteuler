f7c8220e56a3744cdc745eb9ae28cf70ba2398f8
qdonnellan <qdonnellan@gmail.com>
2014-03-19 20:27:49 -0500

    reform mated this test

--- python/problems_01_25/tests/problem_01_test.py
+++ python/problems_01_25/tests/problem_01_test.py
@@ -1,13 +1,15 @@
 from problems_01_25.problem_01 import sum_three_five
 import unittest
 
-class ProblemOneTest(unittest.TestCase):
+class Problem01Test(unittest.TestCase):
     """
     test the implementation of problem one
     """
 
     def test_numbers_less_than_equal_to_ten(self):
         """
+        test problem1 case where limit is 10
+
         there are 4 multiples of 3 or 5 less than 10,
         their sum is 23
         """
