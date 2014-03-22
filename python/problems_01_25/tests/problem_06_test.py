287d5c65c7552b6806db1ca7953a887f2bd06dc0
qdonnellan <qdonnellan@gmail.com>
2014-03-22 10:11:38 -0500

    added problem 6 test

---
+++ python/problems_01_25/tests/problem_06_test.py
@@ -0,0 +1,19 @@
+from problems_01_25.problem_06 import sum_squared_less_squared_sum
+import unittest
+
+class Problem06Test(unittest.TestCase):
+    """
+    test the implementation of Project Euler Problem 6
+    """
+
+    def test_first_ten_ints(self):
+        """
+        the square of the first 10 ints' sum is:
+        (1 + 2 + ... + 10)^2 = 552 = 3025
+
+        the sum of the square of first 10 ints is:
+        1^2 + 2^2 + ... + 10^2 = 385
+
+        thus, the difference should be 3025-385 = 2640
+        """
+        self.assertEqual(sum_squared_less_squared_sum(10), 2640)
