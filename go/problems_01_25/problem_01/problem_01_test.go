0f8a3f9c485158f70bda858a59bb41a999fd3832
qdonnellan <qdonnellan@gmail.com>
2014-03-22 16:33:49 -0500

    minor change to a comment here

--- go/problems_01_25/problem_01/problem_01_test.go
+++ go/problems_01_25/problem_01/problem_01_test.go
@@ -3,7 +3,7 @@ package problem_01
 import "testing"
 
 func TestProblem01(t *testing.T) {
-    // first: testing integers less than 10
+    // testing integers less than 10
     // there are 4 multiples of 3 or 5 less than 10,
     // their sum is 23
     var result int
