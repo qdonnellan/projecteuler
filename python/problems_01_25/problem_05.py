d9bdc55133f85a2c9f6fc7aeeaefc82214b517f4
qdonnellan <qdonnellan@gmail.com>
2014-03-22 09:59:09 -0500

    refactored problem 5, added some better documentation

--- python/problems_01_25/problem_05.py
+++ python/problems_01_25/problem_05.py
@@ -1,18 +1,20 @@
-# 2520 is the smallest number that can be divided by each of the numbers 
-# from 1 to 10 without any remainder.
+def natural_num_division_streak(max_N):
+    """
+    return the largest number perfectly divisivbly by each
+    of the natural numbers 1..max_N
+    """
 
-# What is the smallest positive number that is evenly divisible by all 
-# of the numbers from 1 to 20?
+    n = max_N
+    i = max_N
+    while i > 0:
+        if  n % i == 0: # see if divisible by i
+            i -= 1 # if so, check the next i
+        else:
+            i = max_N # if not, reset the search
+            n += max_N # increment by the next number*
+    return n
 
-n = 20
-i = 20
-while i > 10:
-    # if the remainder is zero search for next lowest until less than 10
-    if  n % i == 0:
-        i -= 1 
-    else:
-    # if the remainder is not zero, restart search at 20 and increment by 20
-        i = 20
-        n += 20
-print n
-
+    """
+    * you should increment by the next number divisible by the
+    largest factor in the search
+    """
