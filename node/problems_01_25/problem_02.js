2faef60c4e507c7be7a5e5488a643e8bdb04a121
qdonnellan <qdonnellan@gmail.com>
2014-03-25 09:08:34 -0500

    added a problem 2 implementation

---
+++ node/problems_01_25/problem_02.js
@@ -0,0 +1,19 @@
+// return the sum of all the even terms in the 
+// Fibonnaci sequence where the largest term in the seq.
+// is less than the limit
+function EvenFibSum(limit) {
+    var sum = 0;
+    var a = 1;
+    var b = 2;
+    while (b < limit) {
+        if (b % 2 == 0) {
+            sum += b;
+        }
+        // a + b evaluales, the a is set to b (a+b) then the value of
+        // the first element (a + b) is returned and set to b
+        b = [a + b, a = b][0];
+    };
+    return sum;
+};
+
+module.exports.EvenFibSum = EvenFibSum;
