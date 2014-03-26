4dd33a91a9fb6e436d65d46fa607b51fe6ef9bd4
qdonnellan <qdonnellan@gmail.com>
2014-03-25 20:30:46 -0500

    added a comment to this test

--- node/test/problem_01_test.js
+++ node/test/problem_01_test.js
@@ -3,6 +3,8 @@ var assert = require("assert");
 
 describe("Problem_01", function() {
     describe("#SumThreeFive()", function() {
+        // The multiples of 3 or 5 less than 10 are:
+        // 3, 5, 6, and 9. Their sum is 23
         it("should return 23 when the input is 10", function() {
             assert.equal(23, prb.SumThreeFive(10));
         });
